cfe70cac6645f6a8daa85167b0c2e1bbaa972569
行云流水justdoit <justdoit>
2018-03-26 20:48:29 +0800

    python

---
+++ python/hello.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+print('hello world')
+print('The quick brown fox','jumps over','the lazy dog')
+print('100 + 200 =',100+200)
+age = input('please enter your age:')
+print('hello,',age)
+print('''line1
+...line2
+....line3''')
+print(r'''hello,\n
+world''')
+print('and',True and True)
+print('or',True or False)
+print('not:',not True)
+if age >= '20':
+    print('adult')
+else:
+    print('teenager')    
