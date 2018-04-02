168f55656e809910de8e4acbdd352f88405c8c36
行云流水justdoit <justdoit>
2018-04-02 21:23:06 +0800

    update

---
+++ python/slots.py
@@ -0,0 +1,17 @@
+class Student(object):
+    pass
+
+s = Student()
+s.name = 'Michael'
+print(s.name)  
+
+def set_age(self,age):
+    self.age = age
+
+from types import MethodType
+s.set_age = MethodType(set_age,s)
+# s.set_age(25)
+# s.age
+
+
+
