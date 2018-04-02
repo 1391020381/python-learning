fafe972d75dc2ff58cf6ffe59e8a52de8d439aaa
行云流水justdoit <justdoit>
2018-04-02 20:57:39 +0800

    update

--- python/class.py
+++ python/class.py
@@ -10,4 +10,25 @@ class Student(object):
 bart = Student('Bart Simpson',59)
 lisa = Student('Lisa Simpson',87)
 bart.print_score()
-lisa.print_score()        
+lisa.print_score()  
+class Animal(object):
+    def run(self):
+        print('Animal is runing.....')
+class Dog(Animal):
+    def run(self):
+        print('Dog is running....')
+
+    def eat(self):
+        print('Eating meat ...')    
+class Cat(Animal):
+     def run(self):
+         print('Cat is running....')  
+dog = Dog()
+dog.run()
+cat = Cat()
+cat.run()  
+def run_twice(animal):
+    animal.run()
+    animal.run()  
+run_twice(Animal())
+run_twice(Dog())                  
